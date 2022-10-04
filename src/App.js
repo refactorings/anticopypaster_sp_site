df769712294c37281b3611fd592cb96465ba7c67
morganwest <mrw6863@g.rit.edu>
2022-10-04 15:56:54 -0400

    feat: :sparkles: got the pages up
    got the pages up
    
    need to style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Links from "./Links";
+import Home from "./Home";
+import {Fragment} from 'react';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+      <Router>
+        <Fragment>
+          <Navbar/>
+          <Routes>
+              <Route exact path='/' element={<Home/>}>
+              </Route>
+              <Route exact path="/links" element={<Links/>}>
+              </Route>
+          </Routes>
+        </Fragment>
+      </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+const navbar= () =>{
+  return (
+  <div>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/Links">Links</Link>
+    </li>
+  </div>
+  );
+}
+export default navbar;
