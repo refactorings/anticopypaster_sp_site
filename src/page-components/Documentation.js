9d2fcc95e0259446f3b07a389621c6e958006be6
Morgan <mrw6863@rit.edu>
2022-10-20 13:21:39 -0400

    feat(Documentation): :sparkles: Add new links to new (and old) documentation

--- src/page-components/Documentation.js
+++ src/page-components/Documentation.js
@@ -7,14 +7,19 @@ function Documentation() {
                 <h3>Project Documentation:</h3>
                 <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1MjN8cIFJxX2y1wPUVOCOWonoWTahiBIBZIv2SXpesTc/edit?usp=sharing">Our Final Plan</a> <br/>
                 <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1PI0WF8ciA6L70ifxCM2B5onAWbS4awFqExqZKKp-aHE/edit?usp=sharing">Our Project Tools and Technologies</a> <br/>
-                <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1vXMzqkiCJqForGTieC9HzBNU_UvUXssrsETK0HIMdcs/edit?usp=sharing">Development Methodology</a>
+                <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1vXMzqkiCJqForGTieC9HzBNU_UvUXssrsETK0HIMdcs/edit?usp=sharing">Development Methodology</a> <br />
+                <a className='DocumentationLinkStyling' href="https://drive.google.com/file/d/11J2y3Fqx97AoB4LBqONpsVgWrMAPAUBU/view?usp=sharing">Wireframes</a> <br />
+                <a className='DocumentationLinkStyling' href="https://drive.google.com/file/d/1fJ87U72qAxtplVoTRQVguHSlRLf9X4nm/view?usp=sharing">UML Diagram</a>
             </div>
 
             <div className='DocumentationBlock'>
                 <h3>Meeting Agendas:</h3>
+                <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1wfgESSq1hSqltaoyY5nvYij4RNrBp4M6SIkl03c8s08/edit?usp=sharing">10/20/2022</a><br />
                 <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1f-az5Ein_bkNgphAl3EXZ32KcNvKrDJTcpoM9uU5eMk/edit?usp=sharing">10/6/2022</a><br />
-                <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1ZSnfqUxowGJcSabjp3dl5CW-auqA3xlHIACWLrAZVVE/edit?usp=sharing">9/29/2022</a>
+                <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1ZSnfqUxowGJcSabjp3dl5CW-auqA3xlHIACWLrAZVVE/edit?usp=sharing">9/29/2022</a><br />
+                <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1VOrSSmaGers3O8Sv_W8HOKeNcKYO-Ip3rDX0VjDN7B8/edit?usp=sharing">9/15/2022</a>
             </div>
+            
         </div>
     )
 }
