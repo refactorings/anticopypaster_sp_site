a699ac170e23a5e168abac9080294bb9e41571d5
Morgan West <mrw6863@rit.edu>
2023-01-26 16:09:37 -0500

    feat(docs, homes, sponsor, team): :lipstick: website style updates
    I put my whole soul into this and I still hate it

--- src/page-components/Home.js
+++ src/page-components/Home.js
@@ -6,26 +6,30 @@ function Home() {
     <div className='Home'>
         {/* TITLE */}
         <div>
-            <h2 className="text-lg font-bold text-gray-900">AntiCopyPaster: Duplicate Code IntelliJ Plugin</h2>
+            <h3 className="font-medium leading-tight text-3xl mt-0 mb-8 text-black">AntiCopyPaster: Duplicate Code IntelliJ Plugin</h3>
         </div>
 
-        {/* PROJECT INFORMATION */}
-        <div className='SynopsisBlock'>
-            <h2 className='font-bold'>Project Synopsis:</h2><br/>
-            <p>The AntiCopyPaster is an IntelliJ plug-in originally created to encourage software developers to use the Refactor menu to avoid duplicating code. The current implementation of the plug-in uses an AI trained model in order to decide if copy/pasted code is similar enough to other code within the file, and shows the user a pop-up to use Extract Method refactoring. The use of the AI model removes the developers from how the plug-in functions and can lead to a lack of trust in the pop-up that occurs. The goal of this senior project is to utilize the metrics currently being used by the AI model and instead allow the user to customize the plugin’s usage of those metrics. This should also have the effect of making the plug-in more lightweight than the current version. All of this code will be contained in a fork of the current AntiCopyPaster plug-in repo and the code will be entirely open-source. The minimum viable product of this senior project is to have a fully functional menu for developers to use with this plug-in that allows them to adjust the sensitivity of the AntiCopyPaster, which will reduce the number of false positive flags. In addition, the stretch goal is to have the AntiCopyPaster search through the entire IntelliJ project rather than just a single file. These contributions shall encourage users to take the pop-ups from the plugin more seriously and increase the use and knowledge of IntelliJ’s refactoring tools among the IntelliJ user base.</p>
-        </div>
+        <div className="inline-flex">
+            {/* PROJECT INFORMATION */}
+            <div className='m-4 p-4 rounded'>
+                <h3 className='font-medium leading-tight text-xl mt-0 mb-4 text-black'>Project Synopsis:</h3>
+                <p>The AntiCopyPaster is an IntelliJ plug-in originally created to encourage software developers to use the Refactor menu to avoid duplicating code. The current implementation of the plug-in uses an AI trained model in order to decide if copy/pasted code is similar enough to other code within the file, and shows the user a pop-up to use Extract Method refactoring. The use of the AI model removes the developers from how the plug-in functions and can lead to a lack of trust in the pop-up that occurs. The goal of this senior project is to utilize the metrics currently being used by the AI model and instead allow the user to customize the plugin’s usage of those metrics. This should also have the effect of making the plug-in more lightweight than the current version. All of this code will be contained in a fork of the current AntiCopyPaster plug-in repo and the code will be entirely open-source. The minimum viable product of this senior project is to have a fully functional menu for developers to use with this plug-in that allows them to adjust the sensitivity of the AntiCopyPaster, which will reduce the number of false positive flags. In addition, the stretch goal is to have the AntiCopyPaster search through the entire IntelliJ project rather than just a single file. These contributions shall encourage users to take the pop-ups from the plugin more seriously and increase the use and knowledge of IntelliJ’s refactoring tools among the IntelliJ user base.</p>
+            </div>
 
-        {/* PROJECT TIMELINE */}
-        <div className='TimelineBlock'>
-            <h3 className="font-bold">Project Timeline:</h3><br/>
-            <div className='MetricsModelPlanBlock'>
-                <a className='DocumentationLinkStyling' href="https://docs.google.com/document/d/1eDYSmdQ1mk26TYgQa8wtxAfeu77ZFJJS7wr-kB5ctbw/edit?usp=sharing">Metrics Model Plan</a>
-            </div>
-            <div className='BackendMetricsModelBlock'>
-                <p className="DocumentationLinkStyling">Backend Metrics Model</p>
-            </div>
-            <div className='FunctioningMetricsMenuBlock'>
-                <p className='DocumentationLinkStyling'>Functioning Metrics Menu</p>
+            {/* PROJECT TIMELINE */}
+            <div className='m-4 p-4 rounded'>
+                <h3 className="font-medium leading-tight text-xl mt-0 mb-2 text-black">Project Timeline:</h3>
+                <div className="flex-col m-2">
+                    <div className=''>
+                        <p className='font-medium leading-tight text-m m-4' href="https://docs.google.com/document/d/1eDYSmdQ1mk26TYgQa8wtxAfeu77ZFJJS7wr-kB5ctbw/edit?usp=sharing">Metrics Model Plan</p>
+                    </div>
+                    <div>
+                        <p className="font-medium leading-tight text-m m-4">Backend Metrics Model</p>
+                    </div>
+                    <div>
+                        <p className='font-medium leading-tight text-m m-4'>Functioning Metrics Menu</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- src/page-components/Sponsor.js
+++ src/page-components/Sponsor.js
@@ -2,11 +2,13 @@ import '../styles/Sponsor.css';
 
 function Sponsor() {
     return (
-        <div className='SponsorBlock'>
+        <div className='SponsorBlock m-2'>
             {/** SPONSOR INFORMATION  */}
-            <h3>Sponsor:</h3>
-            <p><b>Organization:</b> Rochester Institute of Technology - Software Engineering Department</p>
-            <p><b>Primary Contact:</b> Mohamed Wiem Mkaouer: <a href="mailto: mwmvse@rit.edu">mwmvse@rit.edu</a></p>
+            <div className='m-2'>
+                <h4 className="font-medium leading-tight text-xl text-black">Project Sponsor:</h4>
+                <p className="text-m m-2"><b>Organization: </b><a href="mailto: Xueling.Zhang@rit.edu">Rochester Institute of Technology - Software Engineering Department</a></p>
+                <p className="text-m m-2"><b>Primary Contact: </b> Mohamed Wiem Mkaouer - <a href="mailto: mwmvse@rit.edu">mwmvse@rit.edu</a></p>
+            </div>
         </div>
     )
 }
