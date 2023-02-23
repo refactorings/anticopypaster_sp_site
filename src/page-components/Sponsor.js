3579caeb30788d6f1bf49e35bffe9799083a7143
Morgan West <mrw6863@rit.edu>
2023-02-23 13:07:11 -0500

    feat: :lipstick: so many styling fixes

--- src/page-components/Sponsor.js
+++ src/page-components/Sponsor.js
@@ -5,9 +5,9 @@ function Sponsor() {
         <div className='SponsorBlock m-2'>
             {/** SPONSOR INFORMATION  */}
             <div className='m-2'>
-                <h4 className="font-medium leading-tight text-xl text-black">Project Sponsor:</h4>
-                <p className="text-m m-2 border-l-4 pl-4"><b>Organization: </b><a href="mailto: Xueling.Zhang@rit.edu">Rochester Institute of Technology - Software Engineering Department</a></p>
-                <p className="text-m m-2 border-l-4 pl-4"><b>Primary Contact: </b> Mohamed Wiem Mkaouer - <a href="mailto: mwmvse@rit.edu">mwmvse@rit.edu</a></p>
+                <h4 className="font-medium leading-tight text-xl text-zinc-600">Project Sponsor:</h4>
+                <p className="text-m m-2 border-l-4 pl-4 text-zinc-600"><b className='text-zinc-600'>Organization: </b><p className='text-zinc-600'>Rochester Institute of Technology - Software Engineering Department</p></p>
+                <p className="text-m m-2 border-l-4 pl-4 text-zinc-600"><b className='text-zinc-600'>Primary Contact: </b> Mohamed Wiem Mkaouer - <a className='text-zinc-600' href="mailto: mwmvse@rit.edu">mwmvse@rit.edu</a></p>
             </div>
         </div>
     )
